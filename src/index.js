f129247e0d93bb9a22f921735e03dc3455dbf475
Will Harris <will.t.harris@protonmail.com>
2020-08-10 15:36:59 -0700

    chore: import tw css and typeface packages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "tailwindcss/dist/base.min.css";
+import "typeface-eczar";
+import "typeface-roboto-mono";
 
 ReactDOM.render(
 	<React.StrictMode>
