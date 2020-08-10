7d44f05843b3714718fc83b8c37b8c0584866730
Will Harris <will.t.harris@protonmail.com>
2020-08-10 09:35:25 -0700

    chore: install & configure twin.macro/emotion

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React from "react";
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
+import tw from "twin.macro";
+import "tailwindcss/dist/base.min.css";
 
 function App() {
-	return <div>From the App component</div>;
+	return <div tw="text-pink-400">From the App component</div>;
 }
 
 export default App;
