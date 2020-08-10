34edf5f119db6af09c54f0da995b98a60e65faa6
Will Harris <will.t.harris@protonmail.com>
2020-08-10 15:05:32 -0700

    feat: initial mobile navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,30 @@
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
+import tw, { css } from "twin.macro";
+
+import { HamburgerMenu } from "./icons/hamburgerMenu";
+
+export const Navbar = () => {
+	return (
+		<header tw="flex pt-10 pr-6 justify-end">
+			<nav
+				tw="mr-3"
+				css={[
+					css`
+						font-family: Poppins;
+						font-size: 12px;
+						font-weight: 500;
+						font-stretch: normal;
+						font-style: normal;
+						line-height: 1.58;
+						letter-spacing: 3px;
+						color: #1e252c;
+					`,
+				]}
+			>
+				MENU
+			</nav>
+			<HamburgerMenu />
+		</header>
+	);
+};
