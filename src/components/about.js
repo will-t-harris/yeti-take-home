ca9aab9f3ccae497ba641b249d526f049e2cbbc8
Will Harris <will.t.harris@protonmail.com>
2020-08-10 16:35:58 -0700

    feat: add about page

--- src/components/about.js
+++ src/components/about.js
@@ -1,8 +1,50 @@
 /** @jsx jsx */
+import React from "react";
 import { jsx } from "@emotion/core";
-import tw from "twin.macro";
-import "tailwindcss/dist/base.min.css";
+import tw, { css } from "twin.macro";
+
+import { PageTitle } from "./pageTitle";
+import { ContentBlock } from "../components/contentBlock";
+import { AppleOffset } from "../components/appleOffset";
+import { PyramidOffset } from "../components/pyramidOffset";
 
 export const About = () => {
-	return <p>From the about component</p>;
+	return (
+		<React.Fragment>
+			<div>
+				<PageTitle titleText="About" />
+				<AppleOffset />
+			</div>
+			<section tw="mt-20">
+				<ContentBlock
+					hasTitle={false}
+					subTitleText="Andy Griffith turned us down."
+					subTitleFontSize={24}
+					bodyFirstParagraph="Army had half a day. Bad news. Andy Griffith turned us down. He didn't like his nice trailer. That's why you always leave a note!"
+				/>
+			</section>
+			<section>
+				<ContentBlock
+					hasTitle={true}
+					titleText="Our vision"
+					titleFontSize={73}
+					subTitleText="Army had half a day. Bad news. Andy Griffith turned us down. He didn't like his trailer."
+					subTitleFontSize={24}
+					bodyFirstParagraph="Army had half a day. Bad news. Andy Griffith turned us down. He didn't like his trailer. That's why you always leave a note! Army had half a day. Bad news. Andy Griffith turned us down. He didn't like his trailer. That's why you always leave a note! No… but I'd like to be asked! Oh, you're gonna be in a coma, all right."
+				/>
+			</section>
+			<PyramidOffset />
+			<section>
+				<ContentBlock
+					hasTitle={true}
+					titleText="Our values"
+					titleFontSize={68}
+					subTitleText="Army had half a day. Bad news. Andy Griffith turned us down. He didn't like his trailer."
+					subTitleFontSize={22}
+					bodyFirstParagraph="Army had half a day. Bad news. Andy Griffith turned us down. He didn't like his trailer. That's why you always leave a note! Army had half a day. Bad news. Andy Griffith turned us down. He didn't like his trailer. That's why you always leave a note! No… but I'd like to be asked! Oh, you're gonna be in a coma, all right."
+					bodySecondParagraph="Army had half a day. Bad news. Andy Griffith turned us down. He didn't like his trailer. That's why you always leave a note! Army had half a day. Bad news. Andy Griffith turned us down. "
+				/>
+			</section>
+		</React.Fragment>
+	);
 };
