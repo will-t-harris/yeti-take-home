360ca73858756ec8fcee336bbc26be0eba244d78
Will Harris <will.t.harris@protonmail.com>
2020-08-10 16:34:54 -0700

    feat: add logo to navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,10 +3,14 @@ import { jsx } from "@emotion/core";
 import tw, { css } from "twin.macro";
 
 import { HamburgerMenu } from "./icons/hamburgerMenu";
+import { SiteLogo } from "./icons/siteLogo";
 
 export const Navbar = () => {
 	return (
 		<header tw="flex pt-10 pr-6 justify-end">
+			<nav>
+				<SiteLogo />
+			</nav>
 			<nav
 				tw="mr-3"
 				css={[
