61c1ad9fa9447b7f6c9b7122d3aa28f83a9f3ab1
Will Harris <will.t.harris@protonmail.com>
2020-08-10 16:57:41 -0700

    chore: react-ify svg fields

--- src/components/icons/hamburgerMenu.js
+++ src/components/icons/hamburgerMenu.js
@@ -6,9 +6,9 @@ export const HamburgerMenu = () => {
 		<svg xmlns="http://www.w3.org/2000/svg" width="27" height="10" fill="none">
 			<path
 				stroke="#000"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				stroke-width="1.5"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				strokeWidth="1.5"
 				d="M11 8.889h15M1 1.111h25"
 			/>
 		</svg>
