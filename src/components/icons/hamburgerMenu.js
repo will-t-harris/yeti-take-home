6c7594a3e3c3efd2586fe9d49d94f5720d3ef4d5
Will Harris <will.t.harris@protonmail.com>
2020-08-10 15:05:44 -0700

    feat: add hamburger icon

---
+++ src/components/icons/hamburgerMenu.js
@@ -0,0 +1,16 @@
+import React from "react";
+import tw from "twin.macro";
+
+export const HamburgerMenu = () => {
+	return (
+		<svg xmlns="http://www.w3.org/2000/svg" width="27" height="10" fill="none">
+			<path
+				stroke="#000"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="1.5"
+				d="M11 8.889h15M1 1.111h25"
+			/>
+		</svg>
+	);
+};
