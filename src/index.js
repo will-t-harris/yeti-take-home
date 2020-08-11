f4ed76f4c02e727b4801bbf6e21b80ba8c275f6d
Will Harris <will.t.harris@protonmail.com>
2020-08-10 21:59:51 -0700

    fix: remove tw import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "tailwindcss/dist/base.min.css";
 import "typeface-eczar";
 import "typeface-roboto-mono";
 
